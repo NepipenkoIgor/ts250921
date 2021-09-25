31155cb22bdfd04eea1efd30cd82991a0cc42e2e
Igor Nepipenko <inepipenko@muellerone.com>
2021-09-25 17:25:38 +0300

    feat(no-ref): types vs interface

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,67 @@
 // import '../assets/css/style.css';
+/* eslint-disable */
 
-import { speakLikeSloth1 } from './module-1';
-import { speakLikeSloth2 } from './module-2';
-import { speakLikeSloth3 } from './module-3';
-import { speakLikeSloth4 } from './module-4';
-import { speakLikeSloth5 } from './module-5';
+/**Object*/
+// interface IPoint {
+// 	x: number;
+// 	y: number;
+// }
+//
+// type TPoint = {
+// 	x: number;
+// 	y: number;
+// };
+//
+// let p: IPoint = {
+// 	x: 1,
+// 	y: 1,
+// };
 
-speakLikeSloth1(['hi', 'all', 'TS', 'is', 'awesome']);
-speakLikeSloth2(['hi', 'all', 'TS', 'is', 'awesome']);
-speakLikeSloth3(['hi', 'all', 'TS', 'is', 'awesome']);
-speakLikeSloth4(['hi', 'all', 'TS', 'is', 'awesome']);
-speakLikeSloth5(['hi', 'all', 'TS', 'is', 'awesome']);
+/*Function */
+
+// interface ISum {
+// 	(x: number, y: number): number;
+// }
+//
+// type TSum =  (x: number, y: number)=> number ;
+//
+// let sum: TSum = (a: number, b: number) => {
+// 	return a + b;
+// };
+
+/*Extend*/
+
+interface IX {
+	x: number;
+}
+
+interface IY {
+	y: number;
+}
+
+type TX = {
+	x: number;
+};
+type TY = {
+	y: number;
+};
+
+type TPoint = TX & IX;
+
+interface IPoint extends IX, TY {}
+
+/*Implement*/
+
+class Point implements IPoint {
+	public x = 1;
+	public y = 2;
+	public z = 2;
+}
+
+type CB = () => number;
+
+let ab: { a: { c: number } } & { b: string; a: { z: string } } = { a: { c: 1, z: 's' }, b: 's' };
+
+interface IPoint {
+	z: number;
+}
